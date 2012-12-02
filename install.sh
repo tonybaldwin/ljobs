93884f4a70d95bea8f89cb8acb934e2f9f7955dd
tony baldwin <tony@tonybaldwin.org>
2012-12-02 10:04:03 -0500

    currency stored in conf, no var on initiate runs help, and other minor changes

--- install.sh
+++ install.sh
@@ -23,9 +23,15 @@ chmod +x $HOME/bin/ljob
 echo "Creating config files ... "
 read -p "Where do you wish to keep your ljobs files? (default ~/.ljobs/ If you choose another directory, do not forget trailing slash.): " ljpath
 read -p "What is your prefered editor? (default /usr/bin/vim): " editor
+read -p "What is your default currency (default USD. Use three letter code, such as USD, EUR, GBP, BRL, CAD): " currency
+echo "For future reference: you must be consistent in the use of a currency, or the math will result in errors.
+ljob will not convert currency for you.
+As such, you should convert payments, invoices, expenses in other currencies to your default.
+For free currency conversions see www.xe.com"
 
 echo "ljpath=$ljpath" >> $HOME/.ljobs.conf
 echo "editor=$editor" >> $HOME/.ljobs.conf
+echo "currency=$currency" >> $HOME/.ljobs.conf
 
 echo "Installation complete."
 
